f462e89ad0cdf8c2e24fc28c3a83d0fb4a4d91e3
Roberto Rodriguez <rrodriguez@incompanysolutions.com>
2011-08-04 21:00:30 -0600

    fix enviroment.rb config gem mongo mapper

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-config.gem "mongo_mapper"
 # Initialize the rails application
 MongoSpineRc5::Application.initialize!
